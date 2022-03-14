d5bbae2d098c487befdf0099f1bfbd09328625d8
nwlandry <nicholas.landry.91@gmail.com>
2022-03-14 15:22:56 -0600

    fix: updated JSON inspector

--- code/inspect_json.py
+++ code/inspect_json.py
@@ -8,10 +8,14 @@ from sympy import hyper
 filename = sys.argv[1]
 
 with open(filename) as file:
+    # load JSON file
     data = json.loads(file.read())
+
+    # load hypergraph attributes
     try:
         hypergraph_attrs = data["hypergraph"]
 
+        # Is a dataset name specified?
         try:
             name = hypergraph_attrs["name"]
         except:
@@ -19,23 +23,34 @@ with open(filename) as file:
     except:
         print("No hypergraph attributes!")
 
+    # Are nodes specified?
     try:
-        nodes = data["nodes"]
+        node_data = data["node-data"]
     except:
         print("No nodes specified!")
 
+    # Are hyperedges specified?
     try:
-        hyperedges = data["hyperedges"]
+        edge_data = data["edge-data"]
 
-        for e in hyperedges:
+    except:
+        print("No hyperedges specified!")
+    
+    try:
+        edges = data["edge-dict"]
+        for e in edges:
+            # are the nodes in the hyperedges in the list of nodes?
+            if e not in edge_data:
+                print(f"edge {e} not in the list of edges.")
             try:
-                members = hyperedges[e]["members"]
+                members = edges[e]
                 for node in members:
-                    if node not in nodes:
+                    if node not in node_data:
                         print(f"Edge {e} contains non-existent node {node}!")
             except:
                 print(f"Edge {e} has no associated members!")
 
     except:
         print("No hyperedges specified!")
-    # print(hyperedges)
+
+print("Inspection complete.")
