736ca3ff9ae80ca28b107ec62a09f2335da77026
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-05-17 14:51:50 -0400

    clean up hyperbard import script

--- code/import_senate-bills.py
+++ code/import_senate-bills.py
@@ -17,24 +17,30 @@ affiliation_names_file = f"label-names-{dataset_name}.txt"
 
 edgelist_filepath = os.path.join(data_folder, dataset_folder, edgelist_file)
 node_names_filepath = os.path.join(data_folder, dataset_folder, node_names_file)
-node_affiliations_filepath = os.path.join(data_folder, dataset_folder, node_affiliations_file)
-affiliation_names_filepath = os.path.join(data_folder, dataset_folder, affiliation_names_file)
+node_affiliations_filepath = os.path.join(
+    data_folder, dataset_folder, node_affiliations_file
+)
+affiliation_names_filepath = os.path.join(
+    data_folder, dataset_folder, affiliation_names_file
+)
 
 H = xgi.read_edgelist(edgelist_filepath, delimiter=",")
 H["name"] = new_dataset_name
 
 node_labels = utilities.readScHoLPLabels(node_names_filepath, two_column=False)
-node_affiliation = utilities.readScHoLPLabels(node_affiliations_filepath, two_column=False)
+node_affiliation = utilities.readScHoLPLabels(
+    node_affiliations_filepath, two_column=False
+)
 
 for id, name in node_labels.items():
     H.nodes[str(id)].update({"name": name})
 
 affiliation_names = []
 with open(affiliation_names_filepath) as label_data:
-        for line in label_data:
-            affiliation_names.append(line.strip("\n"))
-             
+    for line in label_data:
+        affiliation_names.append(line.strip("\n"))
+
 for id, label in node_affiliation.items():
-    H.nodes[str(id)].update({"affiliation": affiliation_names[int(label)-1]})
+    H.nodes[str(id)].update({"affiliation": affiliation_names[int(label) - 1]})
 
 xgi.write_json(H, os.path.join(data_folder, dataset_folder, f"{new_dataset_name}.json"))
